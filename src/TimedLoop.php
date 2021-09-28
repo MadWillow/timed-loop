d99ed3ee479f92cf5f30b010254638e211431f33
D. S. Richard Richter <richter@vivaldi.net>
2021-09-28 23:33:32 +0200

    style: apply coding guidelines

--- src/TimedLoop.php
+++ src/TimedLoop.php
@@ -1,4 +1,7 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
+
 namespace Rikta\TimedLoop;
 
 class TimedLoop
@@ -24,59 +27,7 @@ class TimedLoop
     }
 
     /**
-     * sets the maximum amount of seconds to run until an exception shall be thrown
-     */
-    public function forMaximumSeconds(float $maxSeconds): self
-    {
-        $this->maxSeconds = $maxSeconds;
-        return $this;
-    }
-
-    /**
-     * sets the value that must be returned by the looped method to keep the loop going
-     */
-    public function untilItReturnsSomethingElseThan($value): self
-    {
-        $this->continueReturn = $value;
-        return $this;
-    }
-
-    /**
-     * sets the amount of microseconds between end of a call and a new try
-     */
-    public function retryingAfterMicroseconds(int $microseconds): self
-    {
-        $this->retryAfterMicroseconds = $microseconds;
-        return $this;
-    }
-
-    /**
-     * disables the throwing of an exception on timeout
-     */
-    public function withoutThrowingException(): self {
-        $this->throwsException = false;
-        return $this;
-    }
-
-    /**
-     * dedicated method for invoking __invoke.
-     * depending on the context it may look weird to calling an object without specifying a method
-     */
-    public function invoke(...$args)
-    {
-        return $this(...$args);
-    }
-
-    /**
-     * static shortcut for the initialisation & execution of a loop with default parameters
-     */
-    public static function loop(callable $callable, ...$args)
-    {
-        return (new self($callable))(...$args);
-    }
-
-    /**
-     * Invokes the registered loop
+     * Invokes the registered loop.
      *
      * executes $this->callable forwarding the arguments 1:1,
      * until it returns something else than $this->continueReturn;
@@ -91,16 +42,70 @@ class TimedLoop
         $end = $start + $this->maxSeconds;
         while (true) {
             $result = ($this->callable)(...$args);
+
             if ($result !== $this->continueReturn) {
                 return $result;
             }
+
             if (($current = microtime(true)) > $end) {
                 if ($this->throwsException) {
                     throw new LoopTimeoutException($current - $start, $this->maxSeconds, $this->continueReturn);
                 }
+
                 return $result;
             }
             usleep($this->retryAfterMicroseconds);
         }
+    }
+
+    /** sets the maximum amount of seconds to run until an exception shall be thrown. */
+    public function forMaximumSeconds(float $maxSeconds): self
+    {
+        $this->maxSeconds = $maxSeconds;
+
+        return $this;
+    }
+
+    /**
+     * dedicated method for invoking __invoke.
+     * depending on the context it may look weird to calling an object without specifying a method.
+     */
+    public function invoke(...$args)
+    {
+        return $this(...$args);
+    }
+
+    /** sets the amount of microseconds between end of a call and a new try. */
+    public function retryingAfterMicroseconds(int $microseconds): self
+    {
+        $this->retryAfterMicroseconds = $microseconds;
+
+        return $this;
+    }
+
+    /**
+     * sets the value that must be returned by the looped method to keep the loop going.
+     *
+     * @param mixed $value
+     */
+    public function untilItReturnsSomethingElseThan($value): self
+    {
+        $this->continueReturn = $value;
+
+        return $this;
+    }
+
+    /** disables the throwing of an exception on timeout. */
+    public function withoutThrowingException(): self
+    {
+        $this->throwsException = false;
+
+        return $this;
+    }
+
+    /** static shortcut for the initialisation & execution of a loop with default parameters. */
+    public static function loop(callable $callable, ...$args)
+    {
+        return (new self($callable))(...$args);
     }
 }
